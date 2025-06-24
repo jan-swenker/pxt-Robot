0325fceeda59fbf026c206c671bea2b2a87fddf4
jan-swenker <104730685+jan-swenker@users.noreply.github.com>
2025-06-24 11:26:19 +0200

    Update Robot.ts

--- Robot.ts
+++ Robot.ts
@@ -2,12 +2,12 @@
  * Custom blocks
 */
 //% color=#2b569b weight=10 icon="\uf544"
-namespace Robot{
-  enum Richting {
+enum Richting {
     Omhoog = 1
     Vooruit = 2
     Omlaag = 3
   }
+namespace Robot{
   //% blockId=Rechter_arm weight=100
   //% block="Rechter arm %richting"
   export function Rechter_arm(Richting): void {
