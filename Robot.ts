18bc52ca88bfec5081a0546107d32dafadd467a1
jan-swenker <104730685+jan-swenker@users.noreply.github.com>
2025-06-24 11:22:30 +0200

    Update Robot.ts

--- Robot.ts
+++ Robot.ts
@@ -3,9 +3,14 @@
 */
 //% color=#2b569b weight=10 icon="\uf544"
 namespace Robot{
+  enum Richting {
+    Omhoog = 1
+    Vooruit = 2
+    Omlaag = 3
+  }
   //% blockId=Rechter_arm weight=100
   //% block="Rechter arm %richting"
-  export function Rechter_arm(richting): void {
+  export function Rechter_arm(Richting): void {
     pins.servoWritePin(AnalogPin.P1, 0)
   }
 }
