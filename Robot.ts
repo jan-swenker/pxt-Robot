020e5524871d6a50a9e7814e8d1023018a6cd69a
jan-swenker <104730685+jan-swenker@users.noreply.github.com>
2025-06-24 17:22:25 +0200

    Update Robot.ts

--- Robot.ts
+++ Robot.ts
@@ -1,16 +1,47 @@
-/**
- * Custom blocks
-*/
-//% color=#2b569b weight=10 icon="\uf544"
+ */
+
 enum Richting {
-    Omhoog = 1,
-    Vooruit = 2,
-    Omlaag = 3
-  }
-namespace Robot{
-  //% blockId=Rechter_arm weight=100
-  //% block="Rechter arm %Richting"
-  export function Rechter_arm(Richting): void {
-    pins.servoWritePin(AnalogPin.P1, 0)
-  }
+    omhoog,
+    vooruit,
+    omlaag
 }
+
+enum Blik {
+    links,
+    vooruit,
+    rechts
+}
+
+//% color="#AA278D" weight=100
+namespace robot {
+    //% block
+    export function rechterarm(kies:Richting):void {
+      if (kies == Richting.omhoog)
+        pins.servoWritePin(AnalogPin.P0,180)
+      else if (kies == Richting.omlaag)
+        pins.servoWritePin(AnalogPin.P0,0)
+      else if (kies == Richting.vooruit)
+        pins.servoWritePin(AnalogPin.P0,90)
+      }
+    //% block
+    export function linkerarm(kies:Richting):void {
+      if (kies == Richting.omhoog)
+        pins.servoWritePin(AnalogPin.P2,0)
+      else if (kies == Richting.omlaag)
+        pins.servoWritePin(AnalogPin.P2,180)
+      else if (kies == Richting.vooruit)
+        pins.servoWritePin(AnalogPin.P2,90)
+      }
+      //% block
+    export function hoofd(kies:Blik):void {
+      if (kies == Blik.links)
+        pins.servoWritePin(AnalogPin.P1,180)
+      else if (kies == Blik.rechts)
+        pins.servoWritePin(AnalogPin.P1,0)
+      else if (kies == Blik.vooruit)
+        pins.servoWritePin(AnalogPin.P1,90)
+      }
+    }
+
+
+
